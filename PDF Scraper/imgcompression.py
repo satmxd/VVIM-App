276b91bd6a04fb00c53c559c7312f9099874ce7a
satmxd <sat.mdyi@gmail.com>
2023-12-12 23:38:23 +0530

    image db add

--- PDF Scraper/imgcompression.py
+++ PDF Scraper/imgcompression.py
@@ -4,13 +4,13 @@ from PIL import Image
 import os
 
 impath = 'D:\\Users\\Satvik\\Documents\\Data\\Studies\\Class 11\\Tree Scanner 23\\Code\\Git\\VVIM-App\\PDF Scraper\\imgs\\'
-savepath = 'D:\\Users\\Satvik\\Documents\\Data\\Studies\\Class 11\\Tree Scanner 23\\Code\\Git\\VVIM-App\\PDF Scraper\\imgs-compressed\\'
+savepath = 'D:\\Users\\Satvik\\Documents\\Data\\Studies\\Class 11\\Tree Scanner 23\\Code\\Git\\VVIM-App\\data\\picdb\\'
 
 arr = os.listdir(impath)
 print(arr)
-qt = 100
+qt = 50
 
-for i in arr[:5]:
+for i in arr:
     img = Image.open(f'{impath}{i}')
     w, h = img.size
     new_size = (w//2, h//2)
