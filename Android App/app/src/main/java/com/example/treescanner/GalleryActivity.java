9752a119fdb516196869ec36cfaa7feba5cb6338
satmxd <sat.mdyi@gmail.com>
2023-12-22 21:56:37 +0530

    gallery fix

--- Android App/app/src/main/java/com/example/treescanner/GalleryActivity.java
+++ Android App/app/src/main/java/com/example/treescanner/GalleryActivity.java
@@ -3,19 +3,41 @@ package com.example.treescanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GalleryActivity extends AppCompatActivity {
+    DBHelper db;
+    Cursor res;
+    List<ImageView> imgviewlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        imgviewlist = new ArrayList<>();
+        imgviewlist.add(0, findViewById(R.id.galleryimg1));
+        imgviewlist.add(1, findViewById(R.id.galleryimg2));
+        imgviewlist.add(2, findViewById(R.id.galleryimg3));
+        imgviewlist.add(3, findViewById(R.id.galleryimg4));
+        imgviewlist.add(4, findViewById(R.id.galleryimg5));
+        imgviewlist.add(5, findViewById(R.id.galleryimg6));
+
 
         ImageView backbtn = findViewById(R.id.backbutton);
+
+        db = new DBHelper(this);
+        res = db.get_data();
+
 
         backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +46,23 @@ public class GalleryActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        update();
+    }
+    private void update(){
+        res.moveToFirst();
+        ArrayList<String> imglist = new ArrayList<String>();
+        while(res.moveToNext()){
+            imglist.add(res.getString(0));
+        }
+        Collections.shuffle(imglist);
+        Collections.shuffle(imgviewlist);
+        int[] pos = {1,2,3};
+        for(int i=0;i<6;i++){
+            String img = imglist.get(i);
+            Collections.shuffle(Collections.singletonList(pos));
+            Glide.with(GalleryActivity.this).load("https://raw.githubusercontent.com/satmxd/VVIM-App/main/data/picdb/"+img+"-"+pos[0]+".png").into(imgviewlist.get(i));
+        }
+
     }
 }
--- Android App/app/src/main/java/com/example/treescanner/SplashScreen.java
+++ Android App/app/src/main/java/com/example/treescanner/SplashScreen.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SplashScreen extends AppCompatActivity {
 Handler handler;
@@ -12,13 +16,17 @@ Handler handler;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        String[] fx = {"Our school has more than 80 different kinds of trees!", "Trees help keep the air cool!", "Trees are known to help reduce stress!", "App works best with Wi-fi enabled!"};
+        Collections.shuffle(Arrays.asList(fx));
         handler = new Handler();
+        TextView txt = findViewById(R.id.splashscreentext);
+        txt.setText(fx[0]);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 finish();
             }
-        }, 1000);
+        }, 2000);
     }
 }
